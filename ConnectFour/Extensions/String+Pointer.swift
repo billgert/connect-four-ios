3fee504517d2a37c2d5961b1df3426427f2ba589
Patrik Billgert <patrik.billgert@gethint.se>
2018-11-27 17:20:44 +0100

    Remove unused observable class

--- ConnectFour/Extensions/String+Pointer.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension String {
-  static func pointer(_ object: AnyObject?) -> String {
-    guard let object = object else { return "nil" }
-    let opaque: UnsafeMutableRawPointer = Unmanaged.passUnretained(object).toOpaque()
-    return String(describing: opaque)
-  }
-}
